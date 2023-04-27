10541f30c214df242ed29035ac22ba384d9d530c
Seongjae <cdaosldk@naver.com>
2023-04-28 01:07:15 +0900

    build

---
+++ src/main/java/com/koreaglobalknowledgenetwork/ysj_board/YsjBoardApplication.java
@@ -0,0 +1,13 @@
+package com.koreaglobalknowledgenetwork.ysj_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsjBoardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YsjBoardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/koreaglobalknowledgenetwork/ysj_board/YsjBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreaglobalknowledgenetwork.ysj_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsjBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
